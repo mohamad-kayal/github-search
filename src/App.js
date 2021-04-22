0e58080bd3ac17b31df8f5a023bf11f7f80553c7
Mohamad Jandali <qaisaljandali944@gmail.com>
2021-04-22 19:53:24 +0200

    "making the components"

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { useState, useEffect } from "react";
 import { render } from 'react-dom';
 import UserData from './UserData/UserData';
@@ -23,5 +24,48 @@ const App = () => {
     </div>
   );
 };
+=======
+import React, { useEffect, useState } from 'react';
+import './App.css';
+//import Person from './Person';
+import SearchForm from './SearchForm';
+
+
+
+function App() {
+  const [persons, setPersons] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('react');
+
+  const GITHUB_API_URL = 'https://api.github.com';
+
+  useEffect(() => {
+    getGithubRepoSearchUrl();
+  }, [])
+
+  function getGithubRepoSearchUrl() {
+    fetch(`${GITHUB_API_URL}/search/repositories?q=repo&page=1&per_page=10`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      //setPersons(data.items)
+      //console.log(data.items);
+    })
+  };
+
+
+  return (
+    <div>
+      <SearchForm 
+        search={search} 
+        setSearch={setSearch} 
+        query={query}
+        setQuery={setQuery}
+      />
+    </div>
+  );
+
+}
+>>>>>>> "making the components"
 
 export default App;

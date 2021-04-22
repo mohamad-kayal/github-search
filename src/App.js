427ad351a31eb0445d0dcacfbfaca72256e08727
Mohamad Jandali <qaisaljandali944@gmail.com>
2021-04-22 23:51:17 +0200

    components and organizing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-//import Person from './Person';
+import Person from './Person';
 import SearchForm from './SearchForm';
 
 
@@ -8,7 +8,8 @@ import SearchForm from './SearchForm';
 function App() {
   const [persons, setPersons] = useState([]);
   const [search, setSearch] = useState('');
-  //const [query, setQuery] = useState('react');
+  // we will use the query state below to search for users and repos keep it in mind ;)
+  // this => const [query, setQuery] = useState();
 
   const GITHUB_API_URL = 'https://api.github.com';
 
@@ -16,29 +17,30 @@ function App() {
     getGithubRepoSearchUrl();
   }, [])
 
-  function getGithubRepoSearchUrl(query) {
+  //Below in our fetch i used default repo instead of ${query} just temporary 
+
+  function getGithubRepoSearchUrl() {
     fetch(`${GITHUB_API_URL}/search/repositories?q=repo&page=1&per_page=10`)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
-      //setPersons(data.items)
-      //console.log(data.items);
+      //console.log(data); this is for testing u call delete it 
+      setPersons(data.items)
     })
   };
 
 
   return (
     <div>
-      <SearchForm 
-        search={search} 
-        setSearch={setSearch} 
+      <SearchForm
+        search={search} //this is a prop
+        setSearch={setSearch} //this is a prop
+      />
+      <Person 
+        persons={persons} //this is a prop
       />
     </div>
   );
 
 }
-//      <Person
-//persons={persons}
-///>
 
 export default App;

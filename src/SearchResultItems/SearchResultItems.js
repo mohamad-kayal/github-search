4ba0fa5f793c87efa5285afd1c8b412b12841229
mohamad-kayal <md.k.1998@hotmail.com>
2021-05-08 15:54:04 +0400

    Working on cards design

--- src/SearchResultItems/SearchResultItems.js
+++ src/SearchResultItems/SearchResultItems.js
@@ -1,58 +1,63 @@
 import React from "react";
 import "./SearchResultItems.css";
+var counter =0;
 export default function SearchResultItems({
   searchResultItems,
   showResults,
-  query
+  query,
 }) {
+  const backgroundPicker = ["#9394c4", "#7879ab", "#6c6d96"];
+  const listColorHandler = () => {
+    if(counter==3)counter=0;
+    return backgroundPicker[counter++];
+  };
 
-  const handleResults = ({ 
+  const handleResults = ({
     owner: { html_url: url },
     owner: { avatar_url },
     full_name: fullName,
     html_url: htmlUrl,
     description,
-   }) => {
-
+  }) => {
     return (
       <div>
         <li style={{ backgroundColor: listColorHandler() }}>
-          <h3>{description}</h3>
-          <a href={htmlUrl}>Repository's Home Page</a>
-          <p>{fullName}</p>
           <img src={avatar_url} />
-          <a href={url}>User profile</a>
+          <h3 className="repo-desc">{description}</h3>
+          <h4>
+            {" "}
+            <a className="repo-anchor" target="_blank" href={htmlUrl}>
+              {fullName}
+            </a>
+          </h4>
+          <h4>
+            <a className="repo-anchor" href={url}>
+              User profile
+            </a>
+          </h4>
         </li>
       </div>
     );
-
-  }
-
-  const style = {
-    backgroundColor: "#eee",
   };
 
-  const listColorHandler = () => {
-    return style.backgroundColor == "#eee"
-      ? (style.backgroundColor = "#bbb")
-      : (style.backgroundColor = "#eee");
-  };
   return (
     <div>
-      { !searchResultItems.length ? <h2>No Results for your search!</h2> :
+      {!searchResultItems.length ? (
+        <h2>No Results for your search!</h2>
+      ) : (
         <div>
-          {showResults && <h3 id="toggleresults">Showing results for: {query}</h3>}
+          {showResults && (
+            <h3 id="toggleresults">Showing results for: {query}</h3>
+          )}
           <div>
             <ul>
-              {searchResultItems.map((searchResultItem) => (
+              {searchResultItems.map(searchResultItem =>
                 handleResults(searchResultItem)
-              ))}
+              )}
             </ul>
           </div>
         </div>
-      }
+      )}
     </div>
   );
 }
-
-//This are the required items from our fetch you can see them in our original js project
